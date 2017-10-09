d0eea392012a1ecbbf24ac6edbe07bd8ad47144c
okaychen <1440281580@qq.com>
2017-10-09 20:45:36 +0800

    完成页面简单布局

---
+++ js/preload.js
@@ -0,0 +1,7 @@
+var ims  = [
+	'http://img2.imgtn.bdimg.com/it/u=1142822403,32080314&fm=214&gp=0.jpg',
+	'http://img2.imgtn.bdimg.com/it/u=1999334205,3363040809&fm=27&gp=0.jpg',
+	'http://p8.aipai.com/photo/179/19254179/photo/68/2653508/2653508_normal.jpg',
+	'http://img5.imgtn.bdimg.com/it/u=3088271132,1257566149&fm=27&gp=0.jpg',
+	'http://img5.duitang.com/uploads/item/201410/24/20141024162359_XmNQJ.jpeg'
+]
