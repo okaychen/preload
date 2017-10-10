3032cbad632acdf992e5c74958fe54bf0bcefa13
okaychen <1440281580@qq.com>
2017-10-10 17:48:14 +0800

    :cn:  complete perload.js with native JS

--- test/js/preload.js
+++ test/js/preload.js
@@ -12,12 +12,37 @@ let imgs = [
 
 let index = 0,
     count = 0,
+    imgWrap = [],
     len = imgs.length,
     $progress = document.querySelector('.progress'),
+    $loading = document.querySelector('.loading'),
     $btn = document.querySelectorAll('.btn'),
     ImgId = document.getElementById('img');
 
 
+// loading
+function preloadImg(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        imgWrap[i] = new Image();
+        imgWrap[i].src = arr[i];
+    }
+}
+preloadImg(imgs);
+
+for (let i in imgWrap) {
+    imgWrap[i].onload = function() {
+        $progress.innerHtml = Math.round((count + 1) / len * 100) + '%';
+        if (count >= len - 1) {
+        	document.title = '1/' + len;
+            $loading.style.display = 'none';
+        }
+        count ++;
+    }
+}
+
+
+
+// switch images
 for (let i in $btn) {
     $btn[i].onclick = function() {
         let dataName = this.getAttribute('data-control')
@@ -26,7 +51,6 @@ for (let i in $btn) {
         } else {
             index = Math.min(len - 1, ++index)
         }
-
         document.title = (index + 1) + '/' + len;
         ImgId.setAttribute('src', imgs[index]);
     }
