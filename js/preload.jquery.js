1ffaf7d58f4c432521ff15d03e19f22ab7d9cc3a
okaychen <1440281580@qq.com>
2017-10-12 23:43:25 +0800

    :+1:  update preload.jquery.js plug-in

--- js/preload.jquery.js
+++ js/preload.jquery.js
@@ -1,16 +1,17 @@
 // 图片预加载
-(function() {
+(function($) {
 
     function PreLoad(imgs, options) {
         this.imgs = (typeof imgs === 'string') ? [imgs] : imgs;
-        this.opts = $.extend({}, PerLoad.DEFAULTS, options);
+        this.opts = $.extend({}, PreLoad.DEFAULTS, options);
         this._unoredered();
-    }
+    };
     PreLoad.DEFAULTS = {
         each: null, // 每一张图片加载完成之后执行
         all: null // 所有图片加载完毕后执行
-    }
-    PerLoad.prototype._unoredered = function() { // 无序加载
+    };
+
+    PreLoad.prototype._unoredered = function() { // 无序加载
         var imgs = this.imgs,
             opts = this.opts,
             count = 0,
@@ -22,7 +23,7 @@
 
             $(imgObj).on('load error', function() {
 
-                opts.each && opts.each();
+                opts.each && opts.each(count);  // 要考虑没有传递参数的情况opts.each === null 
 
                 if (count > len - 1) {
                     opts.all && opts.all();
@@ -34,8 +35,8 @@
     };
 
     $.extend({
-        perload: function(imgs, opts) {
-            new PerLoad(imgs.opts);
+        preload: function(imgs, opts) {
+            new PreLoad(imgs.opts);
         }
     });
 
